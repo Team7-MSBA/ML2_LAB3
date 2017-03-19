1e14e6f09c5f11efdf1af610c0b401f1c0483cd9
vpolakunnil <vpolakunnil@email.wm.edu>
2017-03-19 16:12:23 -0400

    Update Lab3_RandomForest.R

--- Lab3_RandomForest.R
+++ Lab3_RandomForest.R
@@ -1,7 +1,6 @@
 cat("\f")
 remove(list = ls())
-set.seed(1)
-setwd("C:/MSBA/Spring2017/MachineLearning2/Lab3")
+#setwd("C:/MSBA/Spring2017/MachineLearning2/Lab3")
 library(randomForest)
 library(tree)
 library(gbm)
@@ -11,8 +10,8 @@ data <- read.csv("lab3data.csv")
 data <- na.omit(data)
 data <- data[-1]
 names(data) <- tolower(names(data)) 
-names(data)
-head(data$churn)
+#names(data)
+#head(data$churn)
 train <-  sample(1:nrow(data), .8 * nrow(data))
 
 data.train <- data[train,]
@@ -24,43 +23,45 @@ str(data)
 #head(data)
 ###### RANDOM FOREST #####
 nvars = seq(2,(ncol(data)-1))
-train.errorsI = rep(NA, length(nvars))
-train.errorsII = rep(NA, length(nvars))
-test.errorsI = rep(NA, length(nvars))
-test.errorsII = rep(NA, length(nvars))
 train.error <- rep(NA, length(nvars))
 test.error <- rep(NA, length(nvars))
+seeds <- c(1,2,3,4,5,6,7,8,9,10,11)
 
 for(i in 1:length(nvars)){
-      rf.fit <- randomForest(churn~., data=data, subset=train, mtry=(i+1), importance =TRUE)
-      pred.rf.train <- predict(rf.fit, newdata=data[train ,])
-      pred.rf.test <- predict(rf.fit, newdata=data[-train ,])
-      
-      train.rf.tables <- table(pred.rf.train, train.churn)
-      test.rf.tables <- table(pred.rf.test, test.churn)
-      
-      train.error[i] <-  (train.rf.tables[2] + train.rf.tables[3])/nrow(data[train,])
-      train.errorsII[i] <- train.rf.tables[3]/(train.rf.tables[3]+train.rf.tables[4])
-      train.errorsI[i] <- train.rf.tables[2]/(train.rf.tables[1]+train.rf.tables[2])
-      
-      test.error[i] <-  (test.rf.tables[2] + test.rf.tables[3])/nrow(data[-train,])
-      test.errorsII[i] <- test.rf.tables[3]/(test.rf.tables[3]+test.rf.tables[4])
-      test.errorsI[i] <- test.rf.tables[2]/(test.rf.tables[1]+test.rf.tables[2])
-      
+      train.errorIn <- rep(NA, length(seeds))
+      test.errorIn <- rep(NA, length(seeds))
+      for(j in 1:length(seeds)){
+            set.seed(seeds[j])
+            rf.fit <- randomForest(churn~., data=data, subset=train, mtry=(i+1), importance =TRUE)
+            pred.rf.train <- predict(rf.fit, newdata=data[train ,])
+            pred.rf.test <- predict(rf.fit, newdata=data[-train ,])
+            
+            train.rf.tables <- table(pred.rf.train, train.churn)
+            test.rf.tables <- table(pred.rf.test, test.churn)
+            
+            train.errorIn[j] <-  (train.rf.tables[2] + train.rf.tables[3])/nrow(data[train,])
+            #train.errorsII[i] <- train.rf.tables[3]/(train.rf.tables[3]+train.rf.tables[4])
+            #train.errorsI[i] <- train.rf.tables[2]/(train.rf.tables[1]+train.rf.tables[2])
+            
+            test.errorIn[j] <-  (test.rf.tables[2] + test.rf.tables[3])/nrow(data[-train,])
+            #test.errorsII[i] <- test.rf.tables[3]/(test.rf.tables[3]+test.rf.tables[4])
+            #test.errorsI[i] <- test.rf.tables[2]/(test.rf.tables[1]+test.rf.tables[2])
+            
+      }
+      print("Im here")
+      test.error[i] <- mean(test.errorIn)
+      train.error[i] <- mean(train.errorIn)
 }
+
 plot(nvars, train.error, type = "b", xlab = "nvars", ylab = "Train MSE", col = "blue", pch = 20)
 plot(nvars, test.error, type = "b", xlab = "nvars", ylab = "Test MSE", col = "blue", pch = 20, las =2)
 
 plot(nvars, train.errorsI, type = "b", xlab = "nvars", ylab = "Train I", col = "blue", pch = 20)
 plot(nvars, train.errorsII, type = "b", xlab = "nvars", ylab = "Train II", col = "blue", pch = 20, las =2)
+################## Final Forest Model
 
 
 
-plot(nvars, test.errorsI, type = "b", xlab = "nvars", ylab = "Test I", col = "blue", pch = 20)
-plot(nvars, test.errorsII, type = "b", xlab = "nvars", ylab = "Test II", col = "blue", pch = 20, las =2)
-
-nvars[which.min(test.errorsII)]
-min(test.errorsII)
 
 
 
